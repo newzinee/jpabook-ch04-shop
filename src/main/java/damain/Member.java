8b0a5860dfd0acc4656a6602f8503db1800fbf9d
newzin <qvo7896@gmail.com>
2019-12-14 23:21:54 +0900

    소스 정리

--- src/main/java/damain/Member.java
+++ src/main/java/damain/Member.java
@@ -12,18 +12,11 @@ public class Member {
     @Id @GeneratedValue
     @Column(name="MEMBER_ID")
     private Long id;
+
     private String name;
 
-    // 연관관계 매핑
-    @ManyToOne
-    @JoinColumn(name="TEAM_ID")
-    private Team team;
-
+    private String city;
     private String street;
     private String zipcode;
 
-    // 연관관계 설정
-    public void setTeam(Team team) {
-        this.team = team;
-    }
 }
